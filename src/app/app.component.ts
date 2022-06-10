daca230c110cef043fca24e3d5f6c94ebb6f4cac
AzharSayyed <azhartsiddiqui1234@gmail.com>
2022-06-10 16:25:58 +0530

    multiple condition or else if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'my-app';
-  // show=true;
-
-  // if we have sring value in our condition
-  show="no"
-
+  color = 'blue';
 }
