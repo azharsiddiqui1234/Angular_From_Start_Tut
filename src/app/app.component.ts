99e57863d095d0fd70d79121a0258385288838c1
AzharSayyed <azhartsiddiqui1234@gmail.com>
2022-06-10 17:16:29 +0530

    loop In angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   title = 'my-app';
-  color = "blue"
+  // simple loop array
+  // users = ['Azhar', 'Anas', 'Shoaib'];
+  users = [
+    {
+      name: 'Azhar',
+      conact: 22323,
+      email: 'abc@abc.com',
+    },
+    {
+      name: 'Anas',
+      conact: 7878,
+      email: 'cbd@abc.com',
+    },
+    {
+      name: 'Shoaib',
+      conact: 8970,
+      email: 'vbd@abc.com',
+    },
+  ];
 }
