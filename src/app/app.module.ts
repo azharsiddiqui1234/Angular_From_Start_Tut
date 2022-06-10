af8658cc16d6e59007eb92196c7ee956b64254f5
AzharSayyed <azhartsiddiqui1234@gmail.com>
2022-06-10 12:45:11 +0530

    Angular Evants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserAuthModule } from './user-auth/user-auth.module';
 
 
 @NgModule({
@@ -10,8 +9,7 @@ import { UserAuthModule } from './user-auth/user-auth.module';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    UserAuthModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
