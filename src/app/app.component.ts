9843b6c34e08b8bc8df57ab789d61fdd46e3cdfa
AzharSayyed <azhartsiddiqui1234@gmail.com>
2022-06-13 11:42:55 +0530

    Advance Pipes In Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
-  data =
-    'What is pipes ye hamare data ko ek form se dusre form me transfer krta h, we only use in pipes in html file we can not use pipes in typescript file';
-  today = Date()
-  
+  data = 'angular pipes';
+  user = {
+    name: 'Azhar Sayyed',
+    age: 22,
+  };
 }
