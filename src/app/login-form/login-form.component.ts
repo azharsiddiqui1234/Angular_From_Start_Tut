c253592d09adcef020e4867a5c198a88942a67da
AzharSayyed <azhartsiddiqui1234@gmail.com>
2022-06-14 00:01:10 +0530

    reactive with full validation

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,FormGroup } from '@angular/forms';
+import { FormControl,FormGroup,Validators } from '@angular/forms';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
@@ -14,11 +14,24 @@ export class LoginFormComponent implements OnInit {
   
 
   loginForm = new FormGroup({
-    user:new FormControl(''),
-    password:new FormControl('')
+    // user:new FormControl('',[Validators.required,Validators.email]),
+    user:new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z]+$')]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)])
   })
 
   loginUser(){
     console.log(this.loginForm.value);
+  }
+
+  get user(){
+    return(
+      this.loginForm.get('user')
+    )
+  }
+
+  get password(){
+    return(
+      this.loginForm.get('password')
+    )
   }
 }
