9618518cad51942e5b682f2c8cb8eda9dd40c601
AzharSayyed <azhartsiddiqui1234@gmail.com>
2022-06-14 17:09:19 +0530

    child routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { NoPageComponent } from './no-page/no-page.component';
@@ -9,6 +11,16 @@ const routes: Routes = [
   {
     component: AboutComponent,
     path: 'about',
+    children: [
+      {
+        path: 'company',
+        component: AboutCompanyComponent,
+      },
+      {
+        path: 'aboutme',
+        component: AboutMeComponent,
+      },
+    ],
   },
   {
     component: UserComponent,
